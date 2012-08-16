bc38603534bc3191b95695eb441650573932a8c9
Jonathan Wight <jwight@mac.com>
2012-08-15 19:16:07 -0700

    Renamed.

---
+++ AppStream/Source/CTimelineTableView.m
@@ -0,0 +1,28 @@
+//
+//  CTimelineTableView.m
+//  AppStream
+//
+//  Created by Jonathan Wight on 8/15/12.
+//  Copyright (c) 2012 toxicsoftware. All rights reserved.
+//
+
+#import "CTimelineTableView.h"
+
+@implementation CTimelineTableView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
